6f1c8d272f89277d66f92c3697bd5fb79cbc4684
beta96 <gmduncrafted@gmail.com>
2024-12-06 22:38:06 +0100

    code update 1
    fixed enemy movement being connected to the game window not game world
    still bugged a little (gotta fix later)
    
    changes in future:
    - better code
    - better everything

--- main.py
+++ main.py
@@ -122,15 +122,19 @@ while running:
     if keys[pygame.K_w] and player_rect.top > 0:
         player_rect.y -= player_stats["speed"]
         background_y += player_stats["speed"]  # Przesuwamy tło w dół
+        enemy_rect.y += player_stats["speed"]
     if keys[pygame.K_s] and player_rect.bottom < SCREEN_HEIGHT:
         player_rect.y += player_stats["speed"]
         background_y -= player_stats["speed"]  # Przesuwamy tło w górę
+        enemy_rect.y -= player_stats["speed"]
     if keys[pygame.K_a] and player_rect.left > 0:
         player_rect.x -= player_stats["speed"]
         background_x += player_stats["speed"]  # Przesuwamy tło w prawo
+        enemy_rect.x += player_stats["speed"]
     if keys[pygame.K_d] and player_rect.right < SCREEN_WIDTH:
         player_rect.x += player_stats["speed"]
         background_x -= player_stats["speed"]  # Przesuwamy tło w lewo
+        enemy_rect.x -= player_stats["speed"]
 
     # Ograniczanie ruchu tła (aby nie wychodziło poza granice)
     background_x = max(min(background_x, 0), SCREEN_WIDTH - background_rect.width)
